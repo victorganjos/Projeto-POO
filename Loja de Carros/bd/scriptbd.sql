a372280b8dda1e6896dbfd105fe623b70e95dc6f
Fabio Martins <fabiomartins1988@gmail.com>
2019-05-27 12:29:56 -0300

    Popula Banco

--- Loja de Carros/bd/scriptbd.sql
+++ Loja de Carros/bd/scriptbd.sql
@@ -32,7 +32,8 @@ marca VARCHAR(80),
 ano VARCHAR(80), 
 cor VARCHAR(80), 
 placa VARCHAR(80), 
-valor_compra double
+valor_compra double,
+situacao varchar(2)
 );
 
 CREATE TABLE ITEMVENDA(
@@ -59,12 +60,41 @@ FOREIGN KEY (fk_itemvenda) REFERENCES ITEMVENDA(id)
 );
 
 INSERT INTO USUARIO(nome,username,senha) VALUE('victor','victor','123');
+INSERT INTO USUARIO(nome,username,senha) VALUE('fabio','fabio','123');
+INSERT INTO USUARIO(nome,username,senha) VALUE('layssa','layssa','123');
 
 INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
 VALUES('tony','none',null,425,35,'5555','rua',90,'sp','sp','bel',4855,'2019-01-01','pf');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('roberto','none',null,36971828810,449519935,'41353829','Marechal Roberto',90,'sp','sp','bel',06767080,'2019-01-01','pf');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('carlos','none',null,36971828811,449519936,'41353820','Marechal Roberto',89,'sp','sp','bel',06767080,'2019-01-01','pf');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
 
-INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra) 
-VALUES ('CORSA','CHEVROLET','2019','ROSA','BYH-5555',70.000);
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('Livraria','Tades',36971828812,null,null,'41353822','Marechal Roberto',70,'sp','sp','bel',06767080,'2019-01-01','pj');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('Silvano','Marcearia',36971828813,null,null,'41353822','Marechal Roberto',70,'sp','sp','bel',06767080,'2019-01-01','pj');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('Segurancas','Seguro',36971828814,null,null,'41353822','Marechal Roberto',70,'sp','sp','bel',06767080,'2019-01-01','pj');
+INSERT INTO CLIENTE(nome,nome_fantasia,cnpj,cpf,rg,telefone,endereco,numero_residencia,estado,cidade,bairro,cep,data_nascimento,tipo)
+VALUES('Belenzinho','Armas Belem',36971828815,null,null,'41353822','Marechal Roberto',70,'sp','sp','bel',06767080,'2019-01-01','pj');
+
+
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('CORSA','CHEVROLET','2019','ROSA','BYH-5555',70.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Uno','Fiat','2001','Preto','BYH-6666',10.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Ká','Ford','2019','Vermelho','BYH-7777',35.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Palio','Fiat','2016','Branco','BYH-8888',27.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Celta','CHEVROLET','2016','verde','BYH-9999',15.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Gol','wolksvagem','2008','branco','BYH-3333',7.000,'a');
+INSERT INTO PRODUTO (modelo,marca,ano,cor,placa,valor_compra,situacao) 
+VALUES ('Golf','wolksvagem','2017','preto','BYH-5555',70.000,'a');
 
 INSERT INTO ITEMVENDA(fk_produto,valor_unitario)
 VALUES (1,30.000);
